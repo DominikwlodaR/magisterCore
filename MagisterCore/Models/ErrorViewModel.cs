85275e5fa65b5698045d12fe2de627e18863c0bc
wlodar <dominik.wlodar99@gmail.com>
2024-05-08 14:34:08 +0200

    First commit

---
+++ MagisterCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MagisterCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
